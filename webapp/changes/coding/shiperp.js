0f1242b0d216e5f40cf7b5463e0c00ea53f58aa2
Paul Wangdali <paulwangdalijr@gmail.com>
2025-02-28 17:12:04 +0000

    screen updates with shipset and rate quote

--- webapp/changes/coding/shiperp.js
+++ webapp/changes/coding/shiperp.js
@@ -49,10 +49,14 @@ sap.ui.define(
                  // Create a JSON model to store the response
                 var oJSONModel = new sap.ui.model.json.JSONModel();
                 var that = this; // Store reference to 'this' for binding later
-                var oAction = oModel.bindContext("com.sap.gateway.srvd.c_salesordermanage_sd.v0001.GetShipset(...)", this.getView().getBindingContext());
+                var oAction = oModel.bindContext("com.sap.gateway.srvd.c_salesordermanage_sd.v0001.GetShipERPData(...)", this.getView().getBindingContext());
                 // Invoke the action
                 oAction.execute().then(function (oResult) {
                     var oData = oAction.getBoundContext().getObject();
+                    that.byId("shiperpCarrier").setValue(oData.Carrier);
+                    that.byId("shiperpPayment").setValue(oData.Billopt);
+
+
                     var oTableShipsetTemplate = new sap.m.ColumnListItem({
                         cells : [
                             new sap.m.Text({
@@ -72,7 +76,11 @@ sap.ui.define(
                         ]
                     });
                     var oTable = that.byId("tableShipset");
-                    oJSONModel.setData({ ShipsetResults: oData.value });
+                    console.log(oData);
+                    console.log(oData.value);
+                    console.log(oData.shipset);
+                    console.log(oData.shipset.value);
+                    oJSONModel.setData({ ShipsetResults: oData.shipset });
                     oTable.setModel(oJSONModel, "shipsetModel");
                     oTable.bindItems({
                         path: "shipsetModel>/ShipsetResults",
@@ -85,8 +93,103 @@ sap.ui.define(
                 });
 
                 let salesOrderContext = this.getView().byId("cus.sd.salesorderv2.manage::SalesOrderManageObjectPage--fe::ObjectPage").getBindingContext();
-                this.byId("shiperpCondition").setValue(salesOrderContext.getProperty("ShippingCondition"));
-                this.byId("shiperpCarrier").setValue(salesOrderContext.getProperty("xERPISxCARRCODE_SDH"));
+                // this.byId("shiperpCondition").setValue(salesOrderContext.getProperty("ShippingCondition"));
+                // this.byId("shiperpCarrier").setValue(salesOrderContext.getProperty("xERPISxCARRCODE_SDH"));
+            },
+
+            onClickRateQuoteButton : function (oEvent) {
+                // if(!this.getDialog){
+                //     this.getDialog = sap.ui.xmlfragment( ".extension.customer.serp.managesalesorder2.changes.fragments.dialogShipERPRates" );
+                // }
+                // this.getDialog.open(); 
+
+                // var oButton = oEvent.getSource(),
+                //     oView = this.getView();
+
+                // if (!this._pDialog) {
+                //     this._pDialog = sap.ui.core.Fragment.load({
+                //         id: oView.getId(),
+                //         name: "sap.m.sample.SelectDialog.Dialog",
+                //         controller: this
+                //     }).then(function (oDialog){
+                //         oDialog.setModel(oView.getModel());
+                //         return oDialog;
+                //     });
+                // }
+
+                // this._pDialog.then(function(oDialog){
+                //     this._configDialog(oButton, oDialog);
+                //     oDialog.open();
+                // }.bind(this));
+
+
+
+                // if(!this.getDialog){
+                //     this.getDialog = this.byId("fragmentShipERPRates");
+                // }
+                // this.getDialog.open(); 
+
+
+                // var that = this;
+                // if (!this.getDialog) {
+                //     sap.ui.core.Fragment.load({
+                //         name: ".extension.customer.serp.managesalesorder2.subSectionShipERPRates",
+                //         controller: this
+                //     }).then(function(oDialog) {
+                //         that.getDialog = oDialog;
+                //         that.getView().addDependent(that.getDialog);
+                //         that.getDialog.open();
+                //     }).catch(function(oError) {
+                //         console.error("Error loading fragment:", oError);
+                //     });
+                // } else {
+                //     this.getDialog.open();
+                // }
+
+                var oModel = this.getView().getModel(); // Get OData V4 model
+                 // Create a JSON model to store the response
+                var oJSONModel = new sap.ui.model.json.JSONModel();
+                var that = this; // Store reference to 'this' for binding later
+                console.log(".extension.customer.serp.managesalesorder2.shiperp.onClickRateQuoteButton");
+                var oAction = oModel.bindContext("com.sap.gateway.srvd.c_salesordermanage_sd.v0001.GetShipERPRates(...)", this.getView().getBindingContext());
+                // Invoke the action
+                oAction.execute().then(function (oResult) {
+                    var oData = oAction.getBoundContext().getObject();
+                    // var oTableShipsetTemplate = new sap.m.ColumnListItem({
+                    //     cells : [
+                    //         new sap.m.Text({
+                    //             text : "{shipsetModel>ShippingPoint}",
+                    //             wrapping : false
+                    //         }),
+                    //         new sap.m.Text({
+                    //             text : "{shipsetModel>INCOTERMSCLASSIFICATION}",
+                    //             wrapping : false
+                    //         }),
+                    //         new sap.m.Text({
+                    //             text : "{shipsetModel>Customer}"
+                    //         }), 
+                    //         new sap.m.Text({
+                    //             text : "{shipsetModel>RequestedDeliveryDate}"
+                    //         })
+                    //     ]
+                    // });
+                    // var oTable = that.byId("tableShipset");
+                    console.log(oData);
+                    console.log(oData.value);
+                    console.log(oData.shipset);
+                    console.log(oData.shipset.value);
+                    // oJSONModel.setData({ ShipsetResults: oData.shipset });
+                    // oTable.setModel(oJSONModel, "shipsetModel");
+                    // oTable.bindItems({
+                    //     path: "shipsetModel>/ShipsetResults",
+                    //     template: oTableShipsetTemplate
+                    // });                   
+
+
+                }).catch(function (oError) {
+                    console.error("Action failed", oError);
+                });
+
             },
 
             // // this section allows to extend lifecycle hooks or override public methods of the base controller
@@ -110,6 +213,7 @@ sap.ui.define(
             // 	 * This hook is the same one that SAPUI5 controls get after being rendered.
             // 	 * @memberOf {{controllerExtPath}}
             // 	 */
+
             	onAfterRendering: function() {
                     const oShipERPPage = this.getView().byId("cus.sd.salesorderv2.manage::SalesOrderManageObjectPage--customer.serp.managesalesorder2.op-subsection-cae9cd41");
 
@@ -123,6 +227,7 @@ sap.ui.define(
 
                     }
             	},
+
             	/**
             	 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
             	 * @memberOf {{controllerExtPath}}
